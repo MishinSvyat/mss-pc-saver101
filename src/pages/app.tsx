442df16ccafd2d5161362485f12f80d66d014135
Svyatoslav <mishinss@vtblife.ru>
2020-12-13 01:53:07 +0300

    feat: fix css

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './app.css';
 
 export const App: React.FC = () => {
+    const [count, setCount] = useState(0);
     return (
         <div>
+            <h1>count {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
             <p className={styles.text}>
                 hello 101 <span className={styles.grey}>grey</span>
             </p>
